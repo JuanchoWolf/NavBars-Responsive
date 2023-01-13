cf4519ed3e5ab63d1bd97044d87021a194b22478
Estéfano Chicco <estefano.chicco@gmail.com>
2023-01-13 12:47:51 -0300

    css & html

--- mid_Sticky_Top/index.js
+++ mid_Sticky_Top/index.js
@@ -1,7 +1,16 @@
+/* Switch Theme*/
 const btnSwitch = document.querySelector('#switch');
 
 btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('darkMode');
-    document.querySelector('.navbar').classList.toggle('darkMode');
-    document.querySelector('footer').classList.toggle('darkMode');
 });
+/**/
+
+/* Menu Bar */
+const menu = document.getElementById('menu');
+const indicador = document.getElementById('indicador');
+const seccion = document.querySelectorAll('.seccion');
+
+let tamagno_Indicador = menu.querySelector('a').offsetWidth;
+indicador.style.width = tamagno_Indicador + 'px';
+/**/
